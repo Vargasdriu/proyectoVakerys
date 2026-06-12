c9ddc043abd09df0fe75d1b3834a8715c639cead
keithrojaso <rojasortunokeith@pedropoveda.edu.bo>
2026-06-12 14:15:36 -0400

    validacion

--- Productos/crearproducto.php
+++ Productos/crearproducto.php
@@ -60,6 +60,55 @@
 </form>
 
 </div>
+<script>
+    var a=document.getElementById("Codigo");
+    var b=document.getElementById("Producto");
+    var c=document.getElementById("Precio");
+    var d=document.getElementById("Detalle");
+    var e=document.getElementById("Costo");
+    var f=document.getElementById("Stock");
+    var ex=/^[0-9]*$/;
+    var expRegNombre=/^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    function validar(){
 
+        if(a.value==""){
+            alert("este campo no puede ir vacio");
+            a.focus();
+            return false;
+        }
+        if(!expRegNombre.exec(a.value)){
+                alert("introduce solo letras");
+                a.focus();
+                return false;
+        }
+         if(b.value==""){
+            alert("este campo no puede ir vacio");
+            b.focus();
+            return false;
+        }
+         if(c.value==""){
+            alert("este campo no puede ir vacio");
+            c.focus();
+            return false;
+        }
+        
+         if(d.value==""){
+            alert("este campo no puede ir vacio");
+            d.focus();
+            return false;
+        }
+         if(e.value==""){
+            alert("este campo no puede ir vacio");
+            e.focus();
+            return false;
+        }
+         if(f.value==""){
+            alert("este campo no puede ir vacio");
+            f.focus();
+            return false;
+        }
+
+    }
+</script>
 </body>
 </html>
