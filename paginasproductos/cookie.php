43be0fd980b30ab85603acfbe12d5b1081a82d3e
ValeriaMunozG <munozguzmanvaleria@pedropoveda.edu.bo>
2026-06-17 15:52:47 -0400

    pagina de producto por producto

---
+++ paginasproductos/cookie.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilosproductos.css">
+    
+</head>
+<body>
+    <?php include_once "../header.php"; ?>
+
+    <section class="principal" >
+        <section class="proc" >
+            <img src="../imagenes/cookieproc.png" alt="">
+            <section class="detalle">
+                <section class="cuadro">
+                    <p>Para una persona</p>
+                    
+                </section>
+                <section class="cuadro">
+                    <p>100gr</p>
+                </section>
+        </section>
+        </section>
+
+
+        <section class="info">
+            <h1>Chocolate chips cookie</h1>
+            <section class="estrelas">
+                <img src="../imagenes/estrella.png" alt="">
+                <img src="../imagenes/estrella.png" alt="">
+                <img src="../imagenes/estrella.png" alt="">
+                <img src="../imagenes/estrella.png" alt="">
+                <img src="../imagenes/estrella.png" alt="">
+            </section>
+            
+            <section class="precio">
+                <h1>Bs. 6</h1>
+                <p>/unidad</p>
+            </section>
+       
+
+        <section class="info2">
+            <h3>Descripción</h3>
+            <p>Deliciosa galleta artesanal preparada con mantequilla, azúcar morena, 
+                vainilla y generosas chispas de chocolate. Horneada hasta alcanzar el
+                equilibrio perfecto entre una textura suave por dentro y ligeramente
+                crujiente por fuera, ofrece un sabor dulce y reconfortante en cada 
+                bocado. Ideal para disfrutar sola o acompañada de tu bebida favorita.</p>
+            
+        </section>
+
+        <section class="pedir">
+            <div class="cantidad">
+            <button>-</button>
+            <span>1</span>
+            <button>+</button>
+            </section>
+            <a href="carrito.php">
+            <section class="carrito">
+                <img src="" alt="">
+                <p>Añadir al carrito · </p>
+                <p>total</p>
+
+            </section>
+            </a>
+        </section>
+    </section>
+     </section>
+</body>
+</html>
