c03d60ff1e5d1a62335a06c74afc5c5550181fc7
brirojas <rojasjaimesbriana@pedropoveda.edu.bo>
2026-08-09 19:28:16 -0400

    Update registroeditarpedidocliente.php

--- Clientes/registroeditarpedidocliente.php
+++ Clientes/registroeditarpedidocliente.php
@@ -33,7 +33,7 @@ WHERE id='$id'";
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Actualizar Pedido</title>
+<title>Actualizar Pedido  Cliente</title>
 
 <style>
 *{
