87f28a6626dc9f7f6f745575acea17da5449117c
keithrojaso <rojasortunokeith@pedropoveda.edu.bo>
2026-09-06 09:23:15 -0400

    la parte de lo de consultar pedido pero no salen los estilos odio todo

--- paginasproductos/productos.php
+++ paginasproductos/productos.php
@@ -85,6 +85,25 @@
 >
 </div>
 
+<div class="s">
+
+    <h2>Consulta el estado de tu pedido</h2>
+
+    <input
+        type="number"
+        id="numeroPedido"
+        placeholder="Número de pedido"
+        min="1"
+    >
+
+    <button type="button" id="consultar">
+        Consultar pedido
+    </button>
+
+    <div id="resultado"></div>
+
+</div>
+
 
 <!-- ==========================================
      JAVASCRIPT
@@ -96,7 +115,8 @@
 <script src="js/productos.js"></script>
 
 <script src="js/carrito.js"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="js/consultar.js"></script>
 
 <?php include '../footer.php'; ?>
 
